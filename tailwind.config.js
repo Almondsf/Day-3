fc585cba0753f6640e625ba93dab35caffdc8934
Almondsf <oloyedeoluwafemi22@gmail.com>
2022-01-03 19:12:07 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [],
+  theme: {
+    fontFamily: {
+      'bai': ['Bai Jamjuree']
+    },
+    extend: {
+      backgroundImage: {
+        'hero-pattern': "url('/images/bg-header-desktop.png')",
+      }
+      
+    },
+  },
+  plugins: [],
+}
